c2eb0903116f16f6992fd728db7b8717e3ba67d8
Abhisheks <abhisheks@aclmobile.com>
2020-08-25 21:19:52 +0530

    first commit

---
+++ src/test/java/com/acl/zipkinserver/ZipkinServerApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.acl.zipkinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
